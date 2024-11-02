82a0a264245ae39b80a901055566600d896a3d3a
Nirajan Gurung <diorrg@outlook.com>
2024-11-02 14:40:54 +0000

    adds css and functioning js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,10 @@
 const container = document.querySelector(".container");
-let numOfDivs = 64;
-for (i = 0; i < numOfDivs; i++) {
+let numOfDivs = 225;
+
+for (i = 1; i <= numOfDivs; i++) {
   const box = document.createElement("div");
   box.classList.add("box");
-  const boxContent = (box.textContent = `box${i}`);
+  //   const boxContent = (box.textContent = `box${i}`);
   container.appendChild(box);
 
   box.addEventListener("mouseover", (e) => {
@@ -19,6 +20,18 @@ const input = document.querySelector(".input");
 const btn = document.querySelector(".btn");
 
 btn.addEventListener("click", () => {
-  const lala = input.value;
-  console.log(lala);
+  const input1 = input.value;
+
+  while (container.hasChildNodes()) {
+    container.removeChild(container.firstChild);
+  }
+
+  numOfDivs = input1 * input1;
+
+  const box1 = document.querySelector(".box");
+
+  const width = 100 / input1;
+  const height = 100 / input1;
+  box1.style.width = `${width}px`;
+  box1.style.height = `${height}px`;
 });
