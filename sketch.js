63693d110ef250e9de7187881fe0fd95dc7d97b2
Nirajan Gurung <diorrg@outlook.com>
2024-11-02 15:19:07 +0000

    complete code but UI needs to be added

--- sketch.js
+++ sketch.js
@@ -7,6 +7,15 @@ for (i = 1; i <= numOfDivs; i++) {
   //   const boxContent = (box.textContent = `box${i}`);
   container.appendChild(box);
 
+  const width = 100 / Math.sqrt(numOfDivs);
+  const height = 100 / Math.sqrt(numOfDivs);
+  console.log("", width, height);
+  box.style.width = `${width}%`;
+  box.style.height = `${height}%`;
+  box.style.borderColor = "black";
+  box.style.border = "solid";
+  box.style.borderWidth = "2px";
+  box.style.boxSizing = "border-box";
   box.addEventListener("mouseover", (e) => {
     let r = Math.floor(Math.random() * 254);
     let g = Math.floor(Math.random() * 254);
@@ -28,10 +37,27 @@ btn.addEventListener("click", () => {
 
   numOfDivs = input1 * input1;
 
-  const box1 = document.querySelector(".box");
+  for (i = 1; i <= numOfDivs; i++) {
+    const box = document.createElement("div");
+    box.classList.add("box");
+    //   const boxContent = (box.textContent = `box${i}`);
+    container.appendChild(box);
 
-  const width = 100 / input1;
-  const height = 100 / input1;
-  box1.style.width = `${width}px`;
-  box1.style.height = `${height}px`;
+    const width = 100 / Math.sqrt(numOfDivs);
+    const height = 100 / Math.sqrt(numOfDivs);
+    console.log("", width, height);
+    box.style.width = `${width}%`;
+    box.style.height = `${height}%`;
+    box.style.borderColor = "black";
+    box.style.border = "solid";
+    box.style.borderWidth = "2px";
+    box.style.boxSizing = "border-box";
+    box.addEventListener("mouseover", (e) => {
+      let r = Math.floor(Math.random() * 254);
+      let g = Math.floor(Math.random() * 254);
+      let b = Math.floor(Math.random() * 254);
+
+      e.target.style.backgroundColor = `rgb(${r},${g},${b})`;
+    });
+  }
 });
